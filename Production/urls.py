f0ea9579d6bae44ca66509e9ddb3da945e8ae3e5
OsamaRiasat <osamariasat@gmail.com>
2021-08-30 12:05:13 +0500

    app added and models done

---
+++ Production/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import *
+
+router = DefaultRouter()
+
+# router.register('Products', ProductViews, basename='Products')
+
+
+urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<str:pk>/', include(router.urls)),
+
+]
