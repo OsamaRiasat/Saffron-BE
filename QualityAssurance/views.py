4b27f7207e8cd78375244d3d2f850d5d4d160744
OsamaRiasat <osamariasat@gmail.com>
2022-08-10 22:36:17 +0500

    Test

--- QualityAssurance/views.py
+++ QualityAssurance/views.py
@@ -22,7 +22,7 @@ from datetime import date
 # Create your views here.
 
 
-#   ----------------------- RM SAMPLE ----------------------
+#   ----------------------- RM SAMPLE CC----------------------
 
 class GRNOListView(APIView):
     def get(self, request):
